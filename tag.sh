368f18dea96b59086ff2cecf5ed2f57999ff9c15
Kevin Chen <kevinmichaelchen@gmail.com>
2022-11-13 23:14:32 -0500

    Shell check

--- tag.sh
+++ tag.sh
@@ -8,20 +8,22 @@ brew list -q gum || brew install gum
 # Choose MAJOR, MINOR, or PATCH
 TYPE=$(gum choose "patch" "minor" "major")
 
-function get_new_tag {
+get_new_tag() {
   # Download SemVer script
   brew list -q semver || brew install ffurrer2/tap/semver
 
   LATEST_TAG=$(git tag | sort -V | grep -E '^v\d' | head -n1)
   echo "Most recent tag is: ${LATEST_TAG}"
-  LATEST_TAG_WITH_NO_LEADING_V=$(sd '^[v]?(.*)+' '$1' <<< $LATEST_TAG)
+  # shellcheck disable=SC2016
+  # shellcheck disable=SC2039
+  LATEST_TAG_WITH_NO_LEADING_V=$(sd '^[v]?(.*)+' '$1' <<< "$LATEST_TAG")
 
   if [ $1 = "major" ]; then
-    NEW_TAG_WITH_NO_LEADING_V=$(semver next major ${LATEST_TAG_WITH_NO_LEADING_V})
+    NEW_TAG_WITH_NO_LEADING_V=$(semver next major "${LATEST_TAG_WITH_NO_LEADING_V}")
   elif [ $1 = "minor" ]; then
-    NEW_TAG_WITH_NO_LEADING_V=$(semver next minor ${LATEST_TAG_WITH_NO_LEADING_V})
+    NEW_TAG_WITH_NO_LEADING_V=$(semver next minor "${LATEST_TAG_WITH_NO_LEADING_V}")
   elif [ $1 = "patch" ]; then
-    NEW_TAG_WITH_NO_LEADING_V=$(semver next patch ${LATEST_TAG_WITH_NO_LEADING_V})
+    NEW_TAG_WITH_NO_LEADING_V=$(semver next patch "${LATEST_TAG_WITH_NO_LEADING_V}")
   else
     echo "Illegal SemVer bump type. Should be major|minor|patch"
     exit 1
@@ -34,19 +36,19 @@ function get_new_tag {
 # Confirm with user
 gum confirm "Create tags?" && get_new_tag "${TYPE}"
 
-function create_tags {
-  PACKAGE_DIRS=$(find . -mindepth 2 -type f -name 'go.mod' -exec dirname {} \; | egrep -v 'tools' | sed 's/^\.\///' | sort)
+create_tags() {
+  PACKAGE_DIRS=$(find . -mindepth 2 -type f -name 'go.mod' -exec dirname {} \; | grep -E -v 'tools' | sed 's/^\.\///' | sort)
   COMMIT_HASH=$(git rev-parse --verify HEAD)
 
   echo "Creating tag for ${NEW_TAG} at commit: ${COMMIT_HASH}"
 
   # Create tag for root module
-  git tag -a "${NEW_TAG}" -m "Version ${NEW_TAG}" ${COMMIT_HASH}
+  git tag -a "${NEW_TAG}" -m "Version ${NEW_TAG}" "${COMMIT_HASH}"
 
   # Create tag for submodules
   for dir in $PACKAGE_DIRS; do
     echo "Creating tag for ${dir}/${NEW_TAG} at commit: ${COMMIT_HASH}"
-    git tag -a "${dir}/${NEW_TAG}" -m "Version ${dir}/${NEW_TAG}" ${COMMIT_HASH}
+    git tag -a "${dir}/${NEW_TAG}" -m "Version ${dir}/${NEW_TAG}" "${COMMIT_HASH}"
   done
 
   sleep 2
