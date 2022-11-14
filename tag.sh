62f04dfaf9574a0a14325d492c97875c532ea642
Kevin Chen <kevinmichaelchen@gmail.com>
2022-11-13 22:56:43 -0500

    Interactive shell script with Gum

--- tag.sh
+++ tag.sh
@@ -1,16 +1,61 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-PACKAGE_DIRS=$(find . -mindepth 2 -type f -name 'go.mod' -exec dirname {} \; | egrep -v 'tools' | sed 's/^\.\///' | sort)
-COMMIT_HASH=$(git rev-parse --verify HEAD)
+# Download Gum
+brew list -q gum || brew install gum
 
-echo "Creating tag for ${TAG} at commit: ${COMMIT_HASH}"
+# Choose MAJOR, MINOR, or PATCH
+TYPE=$(gum choose "major" "minor" "patch")
 
-# Create tag for root module
-git tag -a "${TAG}" -m "Version ${TAG}" ${COMMIT_HASH}
+function get_new_tag {
+  echo "woohoo $1"
 
-# Create tag for submodules
-for dir in $PACKAGE_DIRS; do
-	git tag -a "${dir}/${TAG}" -m "Version ${dir}/${TAG}" ${COMMIT_HASH}
-done
+  # Download SemVer script
+  brew list -q semver || brew install ffurrer2/tap/semver
+
+  LATEST_TAG=$(git tag | sort -V | grep -E '^v\d' | head -n1)
+  echo "Most recent tag is: ${LATEST_TAG}"
+  LATEST_TAG_WITH_NO_LEADING_V=$(sd '^[v]?(.*)+' '$1' <<< $LATEST_TAG)
+
+  if [ $1 = "major" ]; then
+    NEW_TAG_WITH_NO_LEADING_V=$(semver next major ${LATEST_TAG_WITH_NO_LEADING_V})
+  elif [ $1 = "minor" ]; then
+    NEW_TAG_WITH_NO_LEADING_V=$(semver next minor ${LATEST_TAG_WITH_NO_LEADING_V})
+  elif [ $1 = "patch" ]; then
+    NEW_TAG_WITH_NO_LEADING_V=$(semver next patch ${LATEST_TAG_WITH_NO_LEADING_V})
+  else
+    echo "Illegal SemVer bump type. Should be major|minor|patch"
+    exit 1
+  fi
+
+  NEW_TAG=v${NEW_TAG_WITH_NO_LEADING_V}
+  echo "New tag will be: ${NEW_TAG}"
+}
+
+# Confirm with user
+gum confirm "Create tags?" && get_new_tag "${TYPE}"
+
+function create_tags {
+  PACKAGE_DIRS=$(find . -mindepth 2 -type f -name 'go.mod' -exec dirname {} \; | egrep -v 'tools' | sed 's/^\.\///' | sort)
+  COMMIT_HASH=$(git rev-parse --verify HEAD)
+
+  echo "Creating tag for ${NEW_TAG} at commit: ${COMMIT_HASH}"
+
+  # Create tag for root module
+  git tag -a "${NEW_TAG}" -m "Version ${NEW_TAG}" ${COMMIT_HASH}
+
+  # Create tag for submodules
+  for dir in $PACKAGE_DIRS; do
+    echo "Creating tag for ${dir}/${NEW_TAG} at commit: ${COMMIT_HASH}"
+    git tag -a "${dir}/${NEW_TAG}" -m "Version ${dir}/${NEW_TAG}" ${COMMIT_HASH}
+  done
+
+  sleep 2
+}
+
+create_tags
+
+# gum spin only works with separate shell scripts
+# see https://github.com/charmbracelet/gum/issues/75
+#gum spin --spinner dot --title "Buying Bubble Gum..." -- log_tag
